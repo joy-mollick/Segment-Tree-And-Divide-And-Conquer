0e1d97c4690dd6cc7771a54eb5a0f28da2e2ce68
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-10-17 13:41:05 -0700

    Update SPOJ-HORRIBLE - Horrible Queries.cpp

--- SPOJ-HORRIBLE - Horrible Queries.cpp
+++ SPOJ-HORRIBLE - Horrible Queries.cpp
@@ -1,102 +1,106 @@
-#include<bits/stdc++.h>
 
-using namespace std;
 
-typedef long long int ll;
+    #include <bits/stdc++.h>
+    #define C 100001
+    using namespace std;
 
-const int mx=1e5;
+    //char Binary[C];
+    typedef long long ll;
+    struct{
+        ll sum;
+        ll prop;
+    }Tree[C*4];
+    int MX;
+    int arr[C];
 
-pair<ll,ll>tree[4*mx+4];
-int arr[mx+1];
-
-void construct_segment_tree(int root_index,int b,int e)
-{
-    if(b==e)// begin and end range is equal means leaf node comes place this value
-    {
-        tree[root_index].second=arr[b];
-        return ;
-    }
-    int left=root_index*2;
-    int right=(root_index*2)+1;
-    int mid=(b+e)>>1;
-    //alter=!alter;// one half will be left range and another half will be right range
-    construct_segment_tree(left,b,mid);// left tree will be construct when left is root and b-mid is range
-    construct_segment_tree(right,mid+1,e);
-
-    tree[root_index].second=(tree[left].second+tree[right].second);
-
-}
-
-ll query(int node, int b, int e, int i, int j, ll carry)
-{
-    if (i > e || j < b)
-        return 0;
-
-    if (b >= i and e <= j)
-        return tree[node].second + carry * (e - b + 1);
-
-    int Left = node << 1;
-    int Right = (node << 1) + 1;
-    int mid = (b + e) >> 1;
-
-    ll p1 = query(Left, b, mid, i, j, carry + tree[node].first);
-    ll p2 = query(Right, mid + 1, e, i, j, carry + tree[node].first);
-
-    return p1 + p2;
-}
-void update(int node, int b, int e, int i, int j, ll x)
-{
-    if (i > e || j < b)
-        return;
-    if (b >= i && e <= j)
-    {
-        tree[node].second += ((e - b + 1) * x);
-        tree[node].first += x;
-        return;
-    }
-    int Left = node * 2;
-    int Right = (node * 2) + 1;
-    int mid = (b + e) / 2;
-    update(Left, b, mid, i, j, x);
-    update(Right, mid + 1, e, i, j, x);
-    tree[node].second = tree[Left].second + tree[Right].second + (e - b + 1) * tree[node].first;
-
-}
-
-int main() {
-    int tc;
-    int s=1;
-    scanf("%d",&tc);
-    while(tc--){
-    int n,q;
-    scanf("%d%d",&n,&q);
-    for(int i=1;i<=4*n+1;i++)
-    {
-        if(i<=n)
-        arr[i]=0;
-
-        tree[i].first=0;
-    }
-   construct_segment_tree(1,1,n);
-   //cout<<tree[1].second<<endl;
-  // printf("Case %d:\n",s);
-   s++;
-   int l,r;
-   ll nwval;
-   int c;
-  // update(1,1,n,2,6);
-   while(q--){
-        scanf("%d",&c);
-       if(c==0){
-        scanf("%d%d%lld",&l,&r,&nwval);
-        //l++;r++;
-        update(1,1,n,l,r,nwval);
-       }
-        else {
-                scanf("%d%d",&l,&r);
-        //l++;r++;
-        printf("%lld\n",query(1,1,n,l,r,0));
+    void segment_tree(int node,int b, int e){
+        if(b >= e){
+            if(b == e)Tree[node].sum = 0;
+            return;
         }
-   }
+        int mid = (b+e)/2, child1 = node*2, child2 = child1+1;
+        segment_tree(child1,b, mid);
+        segment_tree(child2,mid+1, e);
+        Tree[node].sum = Tree[child1].sum + Tree[child2].sum;
     }
-   }
+
+    void update(int node,int b, int e, int i, int j,ll val){
+        if(Tree[node].prop!=0)// These will be upper updation before going there
+        {
+            Tree[node].sum+=(e-b+1)*Tree[node].prop;
+            if(b!=e)
+            {
+                Tree[2*node].prop+=Tree[node].prop;
+                Tree[2*node+1].prop+=Tree[node].prop;
+            }
+            Tree[node].prop=0;
+        }
+        if(b > e)return;
+        if(b > j || e < i)return;
+        if(b >= i && e <= j){
+            Tree[node].sum += (e-b+1)*val;
+            if(b!=e)
+            {
+                Tree[2*node].prop+=val;
+                Tree[2*node+1].prop+=val;
+            }
+            return;
+        }
+        int mid = (b+e)/2, child1 = node*2, child2 = child1+1;
+        update(child1,b, mid, i, j,val);
+        update(child2,mid+1, e, i, j,val);
+        Tree[node].sum = Tree[child1].sum + Tree[child2].sum ;
+    }
+
+    ll query(int node,int b, int e,  int i,int j){
+
+        if(Tree[node].prop!=0)// These will be upper updation before going there
+        {
+            Tree[node].sum+=(e-b+1)*Tree[node].prop;
+            if(b!=e)
+            {
+                Tree[2*node].prop+=Tree[node].prop;
+                Tree[2*node+1].prop+=Tree[node].prop;
+            }
+            Tree[node].prop=0;
+        }
+
+        if(b > e)return 0;
+
+        if (i > e || j < b) return 0;
+
+        if (b >= i and e <= j)
+        {
+            return Tree[node].sum;// return that value
+        }
+
+        int mid = (b+e)/2, child1 = node*2, child2 = child1+1;
+        ll x = query(child1,b, mid, i,j);
+        ll y = query(child2,mid+1, e,i,j);
+        return x + y;
+    }
+
+    int main(){
+            int tc;
+            cin>>tc;
+            while(tc--){
+                    memset(Tree,0,sizeof(Tree));
+            int n,c,i,j,Q;
+            ll nwval;
+            cin>>n>>Q;
+            segment_tree(1,1, n);
+            while(Q--){
+               cin>>c;
+                if(c==0){
+                    cin>>i>>j>>nwval;
+                    update(1,1, n, i, j,nwval);
+                }
+                else{
+                    cin>>i>>j;
+                    cout<<query(1,1, n, i,j)<<endl;
+                }
+            }
+            }
+            return 0;
+        }
+
